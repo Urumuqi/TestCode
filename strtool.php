95d39d1b2878a82242e877e34edd1ab7428c43b3
wuqi <yuri1308960477@gmail.com>
2019-04-01 17:34:30 +0800

    update

--- strtool.php
+++ strtool.php
@@ -5,17 +5,6 @@
  * @author wq <yuri1308960477@gmail.com>
  */
 
-// $date = date('Y-m-d H:i:s');
-// $newDate = date('Y-m-d H:i:s', strtotime('-3 month', strtotime($date)));
-// echo $newDate . PHP_EOL;
-
-// echo json_encode($p) . PHP_EOL;
-// 佛山市承埔科技有限公司(中山市)
-// 海南蜂窝科技有限公司(海口市)
-// 佛山通路网络服务有限公司(佛山市)
-// $sql = "SELECT store_id, online_time FROM bi_jiedian.dim_jiedian_store WHERE i_rep_date = 20180424 AND store_id IN ('0000031c-01bf-4a8c-a97d-78be5e4504e6', '00002c13-ab8b-4cb4-bdd9-886b6c4d9e8','00008371-c72e-4e38-831c-d546d95e816b')";
-// $url = 'http://172.21.64.133:18011/hive/handleSqlApi?db=bi_jiedian&sql=' . urlencode($sql) . '&username=caiwu&token=bb9fe6a0-9e3d-423f-85d0-6b4be2b9a394&format=json';
-
 // $curDate = '20180331';
 // $beforeMonth = date('Ymd', strtotime('-1 month', strtotime($curDate)));
 // echo 'current : ' . $curDate . ' | history : ' . $beforeMonth . PHP_EOL;
@@ -238,7 +227,6 @@
 // for ($i = 'a'; $i < 'z'; $i ++) {
 //     echo $i . '-';
 // }
-<<<<<<< HEAD
 // echo strtotime('2018-09-01') .  PHP_EOL;
 // echo date('Y-m-d H:i:s', 1534902586) . PHP_EOL;
 
@@ -328,7 +316,6 @@ var_dump($t);
 // var_dump($offlineRefundInfo);
 // T052DS11RW
 // T125DS12ZA
-=======
 
 // test array_merge
 // $a1 = [
