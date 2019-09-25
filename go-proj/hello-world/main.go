a1bf608eeff077f7de513f744b065b4089f077f1
wuqi <wuqi226@gmail.com>
2019-09-25 15:05:53 +0800

    udpate golang

--- go-proj/hello-world/main.go
+++ go-proj/hello-world/main.go
@@ -1,12 +1,11 @@
 package main
 
-import "fmt"
-
-func swap(x, y string) (string, string) {
-	return y, x
-}
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	args := os.Args
+	fmt.Println(args[0:1])
 }
